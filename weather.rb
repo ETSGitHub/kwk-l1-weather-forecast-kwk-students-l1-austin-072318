925befce03ecb1689ee67136911c6b9a1047b8f2
ETSGitHub <etschulte@gmail.com>
2018-07-26 14:37:24 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -42,6 +42,7 @@ def convert_temp(nyc_weather)
   weather = {:city => "New York", :description => "Partly Cloudy",
 :temperature_farenheit => "66.3"}
 convert_temp = (":temperature_farenheit" -@ 32) * (5.0/9.0)
+end
 temperature_celcius = convert_temp
 temperature_celcius.to_f.round(2)
 puts temperature_celcius
